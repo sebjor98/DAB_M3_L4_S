a18fd5a2cd3a72e96a8ee6ee4822c1c2db93bbff
sebjor98 <sebastian.bjornhom@gmail.com>
2024-04-26 11:22:19 +0200

    fixed the reservation function

--- services/RoomService.js
+++ services/RoomService.js
@@ -1,50 +1,57 @@
+const { sequelize } = require("../models");
+
 class RoomService {
-    constructor(db) {
-        this.client = db.sequelize;
-        this.Room = db.Room;
-        this.Reservation = db.Reservation;
-    }
+  constructor(db) {
+    this.client = db.sequelize;
+    this.Room = db.Room;
+    this.Reservation = db.Reservation;
+  }
 
-    async create(capacity, pricePerDay, hotelId) {
-        return this.Room.create(
-            {
-                Capacity: capacity,
-                PricePerDay: pricePerDay, 
-                HotelId: hotelId
-            }
-        ) 
-    }
-    async get() {
-        return this.Room.findAll({
-            where: {
-            }
-        })
-    }
-    async getHotelRooms(hotelId) {
-        return this.Room.findAll({
-            where: {
-                HotelId: hotelId
-            }
-        })
-    }
+  async create(capacity, pricePerDay, hotelId) {
+    return this.Room.create({
+      Capacity: capacity,
+      PricePerDay: pricePerDay,
+      HotelId: hotelId,
+    });
+  }
+  async get() {
+    return this.Room.findAll({
+      where: {},
+    });
+  }
+  async getHotelRooms(hotelId) {
+    return this.Room.findAll({
+      where: {
+        HotelId: hotelId,
+      },
+    });
+  }
 
-    async deleteRoom(roomId) {
-        return this.Room.destroy({
-            where: {id: roomId}
-        })
-    }
+  async deleteRoom(roomId) {
+    return this.Room.destroy({
+      where: { id: roomId },
+    });
+  }
 
-    async rentARoom(userId, roomId, startDate, endDate) {
-        return this.Reservation.create(
-            {
-                RoomId: roomId,
-                UserId: userId,
-                StartDate: startDate,
-                EndDate: endDate
-            }
-        ).catch(function (err) {
-            console.log(err);
-        });
-    }
+  async rentARoom(userId, roomId, startDate, endDate) {
+    sequelize
+      .query(
+        "CALL insert_reservation(:UserId, :RoomId, :StartDate, :EndDate)",
+        {
+          replacements: {
+            RoomId: roomId,
+            UserId: userId,
+            StartDate: startDate,
+            EndDate: endDate,
+          },
+        }
+      )
+      .then((result) => {
+        return result;
+      })
+      .catch((err) => {
+        return err;
+      });
+  }
 }
 module.exports = RoomService;
