100972dc340f84c4eddd41aa86d95e9854870922
sebjor98 <sebastian.bjornhom@gmail.com>
2024-08-31 08:55:31 +0200

    commit

--- public/javascripts/makeRate.js
+++ public/javascripts/makeRate.js
@@ -1,6 +1,6 @@
 async function makeRate(userId, url) {
     let value = prompt("Rate the hotel from 1 to 5")
-    const response = await fetch(url, {
+    await fetch(url, {
         method: 'POST',
         headers: {
             'Content-type': 'application/json'
@@ -9,8 +9,17 @@ async function makeRate(userId, url) {
             UserId: userId,
             Value: value
         })
-    });
-    const resData = 'Made a rate';
-    location.reload()
-    return resData;
+    }).then((response) => {
+        if (response.ok) {
+            const resData = 'Made a rate';
+            alert(resData);
+            location.reload()
+            return Promise.resolve(resData);
+        }
+        return Promise.reject(response);
+    })
+        .catch((response) => {
+            console.log(response);
+            alert(response.statusText);
+        });
 }
